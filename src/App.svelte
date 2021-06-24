5ccbc7cac9dca3e011dd9e8fbfc6a0033078a33e
Edd224 <84470779+Edd224@users.noreply.github.com>
2021-06-24 11:58:53 +0200

    Svelte

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	let bookTitle = "I'm gonna shoot my dick off, with guns"
+	let times = 1
+</script>
+
+
+
+<header>
+	<h1>Hemi Rotator</h1>
+</header>
+
+<main>
+	<h2>{(bookTitle + ' ').repeat(times)}</h2>
+
+
+	<section class="control">
+		<button on:click={() => times++}>DO IT</button>
+	</section>
+</main>
+<link rel='stylesheet' href='/global.css'>
+<link rel='stylesheet' href='/build/bundle.css'>
+<style>
+	h1{
+		color: red;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+	}
+</style>
+
+
+
+
+<footer class="water">
+
+</footer>
+
+
